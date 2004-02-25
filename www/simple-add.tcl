2268209be6daf7acf2be990a1b32de5024ba3f48
Jeff Davis <davis@xarg.net>
2004-02-25 18:01:38 +0000

    dont use fs::simple_get_type_pretty_name bug 1319 patch 407    Nick Carroll

--- www/simple-add.tcl
+++ www/simple-add.tcl
@@ -1,7 +1,7 @@
 ad_page_contract {
     page to add a new nonversioned object to the system
 
-    @author Ben Adida (ben@openforce.net)    
+    @author Ben Adida (ben@openforce.net)
     @author arjun (arjun@openforce.net)
     @creation-date 01 April 2002
     @cvs-id $Id$
@@ -15,9 +15,9 @@ ad_page_contract {
     {lock_title_p 0}
 } -validate {
     valid_folder -requires {folder_id:integer} {
-	if ![fs_folder_p $folder_id] {
-	    ad_complain "[_ attachments.lt_The_specified_parent_]"
-	}
+        if ![fs_folder_p $folder_id] {
+            ad_complain "[_ attachments.lt_The_specified_parent_]"
+        }
     }
 } -properties {
     folder_id:onevalue
@@ -30,13 +30,12 @@ ad_require_permission $folder_id write
 
 # set templating datasources
 
-set pretty_name [fs::simple_get_type_pretty_name -type $type]
+set pretty_name "URL"
 if {[empty_string_p $pretty_name]} {
     return -code error "[_ attachments.No_such_type]"
 }
 
-set context [list [_ attachments.Attach_URL]]
-set fs_context_bar [fs_context_bar_list -final "[_ attachments.Add_pretty_name]" $folder_id]
+set context [fs_context_bar_list -final [_ attachments.Add_pretty_name [list pretty_name $pretty_name]] $folder_id]
 
 # Should probably generate the item_id and version_id now for
 # double-click protection
