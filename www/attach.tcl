536bc045d10e2c59c9c3c30fa588381e7e269ae7
Hector Romojaro <hector.romojaro@gmail.com>
2019-07-01 14:47:06 +0000

    Move 'util::content_size_pretty' into 'acs-lang' and rename it to 'lc_content_size_pretty', localicing 'bytes' and adapting automated tests. Bump 'acs-lang' version.

--- www/attach.tcl
+++ www/attach.tcl
@@ -59,7 +59,7 @@ db_multirow -unclobber -extend {name_url action_url} contents select_folder_cont
         set name_url [export_vars -base "attach" { {folder_id $object_id} {object_id $to_object_id} return_url pretty_object_name}]
     } else {
         if {$content_size ne ""} {
-            set content_size [util::content_size_pretty -size $content_size]
+            set content_size [lc_content_size_pretty -size $content_size]
         }
         set action_url [export_vars -base "attach-2" {{item_id $object_id} {object_id $to_object_id} return_url pretty_object_name}]
     }
